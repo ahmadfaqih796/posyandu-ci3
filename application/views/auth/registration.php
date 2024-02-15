e7e5752c491ff39d2818f620e79f19d73887c4aa
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-02-15 19:36:10 +0700

    registration

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -12,17 +12,21 @@
                   </div>
                   <form class="user" method="post" action="<?= base_url('auth/registration') ?>">
                      <div class="form-group">
-                        <input type="text" class="form-control form-control-user" name="name" id="name" placeholder="full name">
+                        <input type="text" class="form-control form-control-user" name="name" id="name" placeholder="full name" value="<?= set_value('name'); ?>">
+                        <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                      </div>
                      <div class="form-group">
-                        <input type="email" class="form-control form-control-user" name="email" id="email" placeholder="Email Address">
+                        <input type="email" class="form-control form-control-user" name="email" id="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
                      </div>
                      <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                           <input type="password" class="form-control form-control-user" name="password1" id="password1" placeholder="Password">
+                           <input type="password" class="form-control form-control-user" name="password1" id="password1" placeholder="Password" value="<?= set_value('password1'); ?>">
+                           <?= form_error('password1', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                         <div class="col-sm-6">
-                           <input type="password" class="form-control form-control-user" name="password2" id="password2" placeholder="Repeat Password">
+                           <input type="password" class="form-control form-control-user" name="password2" id="password2" placeholder="Repeat Password" value="<?= set_value('password2'); ?>">
+                           <?= form_error('password2', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                      </div>
                      <button type="submit" class="btn btn-primary btn-user btn-block">

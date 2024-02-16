3f0d117681ff075b6dbbed4bc1e6b4c799c152ea
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-02-16 13:54:30 +0700

    topbar

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,81 +1,39 @@
-<!-- Content Wrapper -->
-<div id="content-wrapper" class="d-flex flex-column">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-   <!-- Main Content -->
-   <div id="content">
+   <!-- Page Heading -->
+   <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
-      <!-- Topbar -->
-      <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-         <!-- Sidebar Toggle (Topbar) -->
-         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-         </button>
-
-         <!-- Topbar Navbar -->
-         <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['name'] ?></span>
-                  <img class="img-profile rounded-circle" src="<?= base_url("assets/img/profile/") . $user['image'] ?>">
-               </a>
-               <!-- Dropdown - User Information -->
-               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                  <a class="dropdown-item" href="#">
-                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                     Profile
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                     Logout
-                  </a>
-               </div>
-            </li>
-
-         </ul>
-
-      </nav>
-      <!-- End of Topbar -->
-
-      <!-- Begin Page Content -->
-      <div class="container-fluid">
-
-         <!-- Page Heading -->
-         <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
-
-         <div class="card mb-3" style="max-width: 540px;">
-            <div class="row no-gutters">
-               <div class="col-md-4">
-                  <img src="<?= base_url("assets/img/profile/") . $user['image'] ?>" class="card-img" alt="...">
-               </div>
-               <div class="col-md-8">
-                  <div class="card-body">
-                     <h5 class="card-title"><?= $user['name'] ?></h5>
-                     <p class="card-text"><?= $user['email'] ?></p>
-                     <p class="card-text"><small class="text-muted">Member since of <?= $user['created_at'] ?></small></p>
-                  </div>
-               </div>
+   <div class="card mb-3" style="max-width: 540px;">
+      <div class="row no-gutters">
+         <div class="col-md-4">
+            <img src="<?= base_url("assets/img/profile/") . $user['image'] ?>" class="card-img" alt="...">
+         </div>
+         <div class="col-md-8">
+            <div class="card-body">
+               <h5 class="card-title"><?= $user['name'] ?></h5>
+               <p class="card-text"><?= $user['email'] ?></p>
+               <p class="card-text"><small class="text-muted">Member since of <?= $user['created_at'] ?></small></p>
             </div>
          </div>
-
       </div>
-      <!-- /.container-fluid -->
-
    </div>
-   <!-- End of Main Content -->
 
-   <!-- Footer -->
-   <footer class="sticky-footer bg-white">
-      <div class="container my-auto">
-         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Posyandu 2020</span>
-         </div>
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+   <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+         <span>Copyright &copy; Posyandu 2020</span>
       </div>
-   </footer>
-   <!-- End of Footer -->
+   </div>
+</footer>
+<!-- End of Footer -->
 
 </div>
 <!-- End of Content Wrapper -->

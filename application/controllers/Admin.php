4c3dca3c6edc8d3fa5f9949c22842981b7424962
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-02-16 15:04:42 +0700

    admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admin extends CI_Controller
+{
+   public function __construct()
+   {
+      parent::__construct();
+      $this->load->library('session');
+   }
+
+   public function index()
+   {
+      $data['user'] =  $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array();
+      $data['title'] = 'Dashboard';
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('admin/index', $data);
+      $this->load->view('templates/footer', $data);
+   }
+}
---
+++ application/views/admin/index.php
@@ -0,0 +1,12 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+   <!-- Page Heading -->
+   <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+
+   <div class="card mb-3" style="max-width: 540px;">
+      Ini admin
+   </div>
+
+</div>
+<!-- /.container-fluid -->

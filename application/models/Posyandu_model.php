7b8195d32f97fb6a9656a2cb103a91ceb3a84f1a
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-02-18 13:48:20 +0700

    posyandu model

---
+++ application/models/Posyandu_model.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Posyandu_model extends CI_Model
+{
+   public function __construct()
+   {
+      parent::__construct();
+   }
+
+   public function get_all_posyandu()
+   {
+      return $this->db->get('posyandu')->result_array();
+   }
+
+   public function get_posyandu_by_id($posyandu_id)
+   {
+      return $this->db->get_where('posyandu', ['id' => $posyandu_id])->row_array();
+   }
+
+   public function add_posyandu($data)
+   {
+      return $this->db->insert('posyandu', $data);
+   }
+
+   public function update_posyandu($posyandu_id, $data)
+   {
+      $this->db->where('id', $posyandu_id);
+      return $this->db->update('posyandu', $data);
+   }
+
+   public function delete_posyandu($posyandu_id)
+   {
+      $this->db->where('id', $posyandu_id);
+      return $this->db->delete('posyandu');
+   }
+}

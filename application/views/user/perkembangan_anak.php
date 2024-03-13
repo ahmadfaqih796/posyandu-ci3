f24f2e18570f0858b51d2893351d37dbb78cb7f7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-03-13 11:48:30 +0700

    perkembangan anak

--- application/views/user/perkembangan_anak.php
+++ application/views/user/perkembangan_anak.php
@@ -1,10 +1,18 @@
 <!-- -------- START PRE-FOOTER 1 w/ SUBSCRIBE BUTTON AND IMAGE ------- -->
 <section class="my-5 pt-5">
    <div class="container">
+      <h4 class="text-center mb-5"><?= $title ?></h4>
       <div class="row">
          <div class="col-md-4 col-sx-12 m-auto">
-            <h4 class="text-center mb-5"><?= $title ?></h4>
-
+            <div class="card">
+               <div class="card-header">
+                  <h4 class="text-center">Timbangan & Perkembangan</h4>
+               </div>
+               <div class="card-body">
+                  <img src="<?= base_url("assets/img/posyandu/p_header.jpeg") ?>" alt="" width="100%">
+                  <a class="btn btn-primary mt-4" href="<?= base_url('user/timbangan') ?>">Lihat</a>
+               </div>
+            </div>
          </div>
          <div class="col-md-4 col-sx-12 m-auto">
             <div class="card">
@@ -16,12 +24,8 @@
                   <a class="btn btn-primary mt-4" href="<?= base_url('user/imunisasi') ?>">Lihat</a>
                </div>
             </div>
-
          </div>
-         <div class="col-md-4 col-sx-12 m-auto">
-            <h4 class="text-center mb-5"><?= $title ?></h4>
 
-         </div>
       </div>
    </div>
 </section>

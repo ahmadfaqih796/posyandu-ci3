d558c8559db65ca2ad564efee5f3599bf2cda584
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2024-08-29 16:18:50 +0700

    logo index

--- application/views/visitor/index.php
+++ application/views/visitor/index.php
@@ -15,7 +15,7 @@
    <!-- Navbar -->
    <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
       <div class="container-fluid">
-         <a class="navbar-brand" href="#">Navbar</a>
+         <img src="<?= base_url(); ?>/assets/img/puskesmas.png" alt="Logo" width="40" height="50" class="d-inline-block align-text-top navbar-brand">
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
          </button>
